99bb748bba0d052a4f34f037ddb5351d4a30044a
Faris-abukhader <70070951+Faris-abukhader@users.noreply.github.com>
2023-01-13 14:51:31 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+from mhtwyat import Mhtwyat
+mhtwyat = Mhtwyat()
+# first this function
+mhtwyat.save_all_articles_title_into_file()
+# then run this function and pass the name of generated file from upon function
+# asyncio.run(mhtwyat.save_articles_into_file('file_name_here.json'))
